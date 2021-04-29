d03eadfddce880afcf9d7081259ccdc9f1842b7f
Adrián <amiralles@iesfuentesanluis.org>
2021-04-29 16:13:21 +0200

    1- He añadido la clase "MaquinaDeBebidas" al proyecto "Maquina3_2020" 2- He añadido la clase "Test_Maquina3" al proyecto "Maquina3_2020" por motivos explicados dentro de esa misma clase, en los comentarios.

---
+++ src/maquina3_2020/MaquinaDeBebidas.java
@@ -0,0 +1,158 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package maquina3_2020;
+
+/**
+ *
+ * @author Adrián Miralles Gómez
+ * Equipo -> máquina 3.
+ */
+public class MaquinaDeBebidas {
+    private ContadorDeMonedas contador; //Contador de monedas de la maquina	
+	private DispensadorDeBotes cola,limon,naranja,tonica,	agua; //Cantidad de botes en el dispensador
+	private float precio;	//Precio de las bebidas(común para todas)
+	
+	/*
+	Inicializa la maquina y todos los elementos asociados
+	Entradas: float m, cantidad inicial de monedas para el	cambio
+	int b, cantidad inicial de botes en los dispensadores
+	float pvp, precio de las bebidas
+	Salidas: Ninguna 
+	*/
+	public MaquinaDeBebidas (float m, int b, float pvp) {
+		contador = new ContadorDeMonedas (m);
+		
+		cola = new DispensadorDeBotes(b);
+		limon = new DispensadorDeBotes(b);
+		naranja =new DispensadorDeBotes(b);
+		tonica = new DispensadorDeBotes(b);
+		agua = new DispensadorDeBotes(b);
+		
+		precio = pvp;
+	}
+	
+	/*
+	Responder a una acción del usuario. Discrimina el tipo de
+	accion
+	y utiliza las operaciones privadas
+	Entradas: char o, la orden del usuario
+	Salidas: Ninguna 
+	*/
+	public void darOrden (char o){
+		if (o=='A' ||o=='B'||o=='C'||o=='D'||o=='E'||o=='R') {
+			ordenMonedas(o);
+		}else if(o=='1' ||o=='2'||o=='3'||o=='4'||o=='5'  ) {
+			ordenSeleccion(o);
+		}	
+	}
+		
+	/*
+	Visualiza la informacion de interes sobre la maquina botes en cada dispensador, precio de las bebidas, saldo del cliente.
+	Entradas: Ninguna
+	Salidas: Ninguna 
+	*/
+	public void visualizarMaquina () {
+		System.out.println("VISUALIZANDO MÁQUINA");
+		System.out.println();
+		System.out.println("Cola: STOCK -> "+ cola.botesDisponibles()+" PRECIO -> "+precio);
+		System.out.println("Limon: STOCK -> "+ limon.botesDisponibles()+" PRECIO -> "+precio);
+		System.out.println("Naranja: STOCK -> "+ naranja.botesDisponibles()+" PRECIO -> "+precio);
+		System.out.println("Tonica: STOCK -> "+ tonica.botesDisponibles()+" PRECIO -> "+precio);
+		System.out.println("Agua: STOCK -> "+ agua.botesDisponibles()+" PRECIO -> "+precio);
+		System.out.println("\nSALDO DISPONIBLE DEL CLIENTE: " + contador.saldo());
+	}
+	
+	private void ordenMonedas (char o) {
+		switch (o) {
+		case 'A': 
+			contador.insertarMoneda(0.05F);
+			break;
+		case 'B':
+			contador.insertarMoneda(0.10F);
+			break;
+		case 'C':
+			contador.insertarMoneda(0.20F);
+
+			break;
+		case 'D':
+			contador.insertarMoneda(0.50F);
+
+			break;
+		case 'E':
+			contador.insertarMoneda(1F);
+			break;
+		case 'R':
+			contador.retornar();
+			break;		
+		}	
+	}
+	
+	/*
+	Realiza las acciones relacionadas con la selección de una bebida 
+	Entradas: char o, la orden del usuario
+	Salidas: Ninguna 
+	*/
+	private void ordenSeleccion (char o) {
+		switch (o) {
+		case '1': 
+			if (cola.pulsarBoton()==true) {
+				if (contador.saldo()>=precio) {
+					cola = new DispensadorDeBotes(cola.botesDisponibles()-1);
+				}
+				System.out.println("CAMBIO -> " +contador.darCambioDe(precio));
+			}else {
+				System.out.println("NO QUEDA STOCK DE COCACOLA");
+			}
+			break;
+			
+		case '2':
+			if (limon.pulsarBoton()==true) {
+				if (contador.saldo()>=precio) {
+					limon = new DispensadorDeBotes(limon.botesDisponibles()-1);
+				}
+				System.out.println("CAMBIO -> " +contador.darCambioDe(precio));
+			}else {
+				System.out.println("NO QUEDA STOCK DE LIMÓN");
+			}
+			break;
+
+		case '3':
+			if (naranja.pulsarBoton()==true) {
+				if (contador.saldo()>=precio) {
+					naranja = new DispensadorDeBotes(naranja.botesDisponibles()-1);
+				}
+				System.out.println("CAMBIO -> " +contador.darCambioDe(precio));
+			}else {
+				System.out.println("NO QUEDA STOCK DE NARANJA");
+			}
+			break;
+			
+		case '4':
+			if (tonica.pulsarBoton()==true) {
+				if (contador.saldo()>=precio) {
+					tonica = new DispensadorDeBotes(tonica.botesDisponibles()-1);
+				}
+				System.out.println("CAMBIO -> " +contador.darCambioDe(precio));
+
+			}else {
+				System.out.println("NO QUEDA STOCK DE TÓNICA");
+			}
+			break;
+			
+		case '5':
+			agua.pulsarBoton();
+			if (agua.pulsarBoton()==true) {
+				if (contador.saldo()>=precio) {
+					agua = new DispensadorDeBotes(agua.botesDisponibles()-1);
+				}
+				System.out.println("CAMBIO -> " +contador.darCambioDe(precio));
+
+			}else {
+				System.out.println("NO QUEDA STOCK DE AGUA");
+			}
+		}
+	}
+}
---
+++ src/maquina3_2020/Test_Maquina3.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package maquina3_2020;
+import java.io.*;
+
+/**
+ *
+ * @author Adrián Miralles Gómez
+ * 
+ * Te hago este main, distinto al que ya está en el repositorio, porque ese tenia algunas cosas mal, pero no quería modificartelo directamente sin decírtelo.
+ * Por ejemplo:
+ * - En la linea 23 creas un objeto de la clase MaquinaBebida, y la clase realmente se llama MaquinaDeBebidas.
+ * - En la linea 30 llamas al metodo de MaquinaBebida "iniciarMaquinaDeBebidas", que no existe en la clase MaquinaDeBebidas, porque esa función la hace el
+ * propio constructor de la clase MaquinaDeBebidas.
+ */
+public class Test_Maquina3 {
+    public static void main(String[] args) throws IOException {
+        //Scanner tec = new Scanner(System.in);
+    	
+        MaquinaDeBebidas mi_maquina;
+        char c;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+        //inicializo la maquina con:
+        //  50 euros para cambio
+        //  25 botes en cada dispensador
+        //  Precio de 0.75 euros por bote
+        mi_maquina = new MaquinaDeBebidas(50F, 25, 0.75F);
+
+        do {
+		mi_maquina.visualizarMaquina ();
+		System.out.println("\nRealice su seleccion (f = apagar maquina) ... ");
+		System.out.println("   ('A'=5 cent, 'B'=10 cent, 'C'=20 cent, 'D'=50 cent, 'E'=1 euro, 'R'=devolver monedas)");
+		System.out.println("   ('1'=cola, '2'=limon, '3'=naranja, '4'=tonica, '5'=agua)");
+		System.out.print("Seleccion ->");
+		c = (char) br.read();
+		mi_maquina.darOrden (c);
+        }
+		while ( ( c != 'f' ) && ( c != 'F' ) );
+    }
+}
